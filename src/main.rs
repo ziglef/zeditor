d27372f9bc795968105806228660443796203950
Grandao <ruigrandaorocha@gmail.com>
2022-11-14 11:38:36 +0000

    disabled terminal echo mode. read user input until they hit q

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::{io::{self, Read}, process::exit, os::unix::prelude::AsRawFd};
+
+
+// We unwrap here since we should panic if we can't change the terminal flags
+// For now we are just disabling echoing in the terminal but eventually we will also enable raw mode
+fn enable_raw_mode() -> () {
+    let mut raw = termios::Termios::from_fd(io::stdin().as_raw_fd()).unwrap();
+
+    raw.c_lflag &= !termios::ECHO;
+
+    termios::tcsetattr(io::stdin().as_raw_fd(), termios::TCSAFLUSH, &raw).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    enable_raw_mode();
+
+    // Main loop to read user input one byte at a time
+    let mut input: char;
+    for byte in io::stdin().bytes() {
+        match byte {
+            Ok(byte_value) => input = char::from(byte_value), 
+            Err(err) => {
+                println!("Error parsing bytes from stdin.\nError code: {}", err);
+                exit(-1);
+            }
+        }
+
+        if input == 'q' {
+            exit(0);
+        }
+
+        println!("{}", input)
+    }
 }
