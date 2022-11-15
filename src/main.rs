b2ae99f72fbc3a4dc1ba76fac961bbfd23fb57d5
Rui Grandao <ruigrandaorocha@gmail.com>
2022-11-15 16:52:58 +0000

    small cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use std::{io::{self, Read}, process::exit, os::unix::prelude::AsRawFd};
 // We unwrap here since we should panic if we can't change the terminal flags
 // For now we are just disabling echoing in the terminal but eventually we will also enable raw mode
 fn enable_raw_mode() -> () {
-    let mut raw = termios::Termios::from_fd(io::stdin().as_raw_fd()).unwrap();
+    let mut termios = termios::Termios::from_fd(io::stdin().as_raw_fd()).unwrap();
 
-    raw.c_lflag &= !termios::ECHO;
+    termios.c_lflag &= !termios::ECHO;
 
-    set_termios(io::stdin().as_raw_fd(), raw);
+    set_termios(io::stdin().as_raw_fd(), termios);
 }
 
 
